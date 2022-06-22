dfcd7f7ec6c6fd2bf4e51ceeb10be4299d5e3af2
Jeremiah Mojica <jrmhmojica@gmail.com>
2022-06-22 20:04:25 +0800

    Changed the unit of temperature from the default 'standard' to 'metric'

--- Weather Fetcher/weather_fetcher.py
+++ Weather Fetcher/weather_fetcher.py
@@ -6,12 +6,12 @@ BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 #GETTING INPUT(CITY) FROM THE USER
 city = input("Enter city: ")
-request_url = f"{BASE_URL}?q={city}&appid={config.API_KEY}"
+request_url = f"{BASE_URL}?q={city}&appid={config.API_KEY}&units=metric"
 response = requests.get(request_url)
 
 if response.status_code == 200:
     data = response.json()
     print("Weather: " + data["weather"][0]["description"])
-    print("Temperature:", round(data["main"]["temp"] - 273.15, 2), "degrees celsius") #not sure about the conversion of farenheit to celsius
+    print("Temperature:", data["main"]["temp"], "degrees celsius")
 else:
     print("An error occurred.")
